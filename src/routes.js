348fcacd15a3885667026cb51a04f1076a4c53ed
darmani0427 <darmani0427@mm.com>
2017-04-08 17:59:37 -0400

    add login screen and post

--- src/routes.js
+++ src/routes.js
@@ -2,4 +2,12 @@ module.exports = function (app) {
     app.get('/', function (req, res) {
         res.render(__dirname + '/views/index.ejs'); 
     })
+
+    app.get('/login', function(req, res) {
+        res.render(__dirname + '/views/login.ejs');
+    })
+
+    app.post('/login', function (req, res) {
+        res.send('hooray login successful!')
+    })
 };
