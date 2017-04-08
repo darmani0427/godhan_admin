e699065c9dd9d2f3b171cd74f30eaa0153fa2846
darmani0427 <darmani0427@mm.com>
2017-04-08 17:41:41 -0400

    added index view

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,5 @@
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.send("Hello")
-    })
-
-    app.get('/login', function (req, res) {
-        res.send('response here with the login page')
+        res.render(__dirname + '/views/index.ejs'); 
     })
 };
