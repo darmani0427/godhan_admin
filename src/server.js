07bdbf1d59019246d482edaccf2701a138b529d3
darmani0427 <darmani0427@mm.com>
2017-04-08 17:27:53 -0400

    added configuration file

--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ var express = require('express')
 var mongoose = require('mongoose')
 var app = express()
 
-require('./routes.js')(app);
+require('./routes.js')(app)
+var configuration = require('./configuration.js')
 
-mongoose.connect("mongodb://localhost:27017/gohdan")
+mongoose.connect(configuration.databaseURL)
 
 app.listen(3000, function() {
     console.log('listening on 3000')
