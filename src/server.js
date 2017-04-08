01eabc37d059d250c850e4d9b7fd4cb644e07ae4
darmani0427 <darmani0427@mm.com>
2017-04-08 17:08:45 -0400

    added login endpoint for later

--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ app.get('/', function(req, res) {
     res.send("Hello")
 })
 
+app.get('/login', function(req, res) {
+    res.send('response here with the login page')
+})
+
 app.listen(3000, function() {
     console.log('listening on 3000')
 })
