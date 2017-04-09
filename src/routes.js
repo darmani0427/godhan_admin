878ef8856b7124f1110e411c401d5fd475f9db5e
darmani0427 <darmani0427@mm.com>
2017-04-09 14:02:33 -0400

    added login screen which redirects to accounts console

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 var Account = require('./account.js')
 
 module.exports = function (app) {
-    app.get('/', function (req, res) {
+    app.get('/', function(req, res) {
+        res.render(__dirname + '/views/index.ejs')
+    })
+
+    app.post('/login', function(req, res) {
+        res.redirect('/accounts')
+    })
+
+    app.get('/accounts', function (req, res) {
         Account.find({}, function (error, documents) {
             console.log(documents)
             res.render(__dirname + '/views/accounts.ejs', {
